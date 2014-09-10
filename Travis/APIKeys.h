87352f2f27235f98f246dc44c309a61a576fbf94
Mohammed Neundorf <mne@damncreative.de>
2014-09-10 12:15:12 +0200

    added header for apikeys

---
+++ Travis/APIKeys.h
@@ -0,0 +1,15 @@
+//
+//  APIKeys.h
+//  Travis
+//
+//  Created by Nax on 10.09.14.
+//  Copyright (c) 2014 Naxmeify. All rights reserved.
+//
+
+#ifndef Travis_APIKeys_h
+#define Travis_APIKeys_h
+
+#define GITHUB_CLIENT_ID @""
+#define GITHUB_CLIENT_SECRET @""
+
+#endif
